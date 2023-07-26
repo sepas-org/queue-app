e5f0f261467672c5c5d32c2b4c130182ffadcb4f
Muhamad Zulfan <73164719+neunicorn@users.noreply.github.com>
2023-07-26 15:28:24 +0700

    login admin and admin schema model

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -1,3 +1,38 @@
-class Admin {}
+const AdminSchema = require("../models/Admin");
+class Admin {
+  async login(req, res) {
+    try {
+      const { username, password } = req.body;
+      if (!username) {
+        throw { code: 400, message: "Username is required" };
+      }
+      if (!password) {
+        throw { code: 400, message: "Password is required" };
+      }
+
+      AdminSchema.find(
+        { username: username, password: password },
+        (err, result) => {
+          if (err) {
+            throw { code: 500, message: err.message };
+          }
+          if (result.length === 0) {
+            throw { code: 404, message: "Username or password is wrong" };
+          }
+          return res.status(200).json({
+            status: true,
+            message: "Login success",
+            data: result,
+          });
+        }
+      );
+    } catch (err) {
+      return res.status(err.code || 500).json({
+        status: false,
+        message: err.message,
+      });
+    }
+  }
+}
 
 module.exports = new Admin();
