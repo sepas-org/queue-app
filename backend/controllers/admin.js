a217be3ab83e7ca0b1d709398e063b23d657f4d9
Muhamad Zulfan <73164719+neunicorn@users.noreply.github.com>
2023-07-27 11:34:10 +0700

    login and register

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -1,5 +1,5 @@
 const AdminSchema = require("../models/Admin");
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 class Admin {
   async register(req, res) {
     try {
@@ -17,10 +17,11 @@ class Admin {
       if (checkUser.length > 0) {
         throw { code: 400, message: "Username already exist" };
       }
-
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
       const admin = new AdminSchema({
         username: username,
-        password: password,
+        password: hashedPassword,
       });
       const saveAdmin = await admin.save();
       return res.status(200).json({
@@ -38,6 +39,7 @@ class Admin {
   async login(req, res) {
     try {
       const { username, password } = req.body;
+      console.log(username);
       if (!username) {
         throw { code: 400, message: "Username is required" };
       }
@@ -45,10 +47,21 @@ class Admin {
         throw { code: 400, message: "Password is required" };
       }
 
-      const checkUser = await AdminSchema.find({
+      const [checkUser, _] = await AdminSchema.find({
         username: username,
       }).exec();
-      console.log();
+
+      const isPasswordValid = await bcrypt.compareSync(
+        password,
+        checkUser.password
+      );
+      if (!isPasswordValid) {
+        throw { code: 400, message: "PASSWORD_INVALID" };
+      }
+      return res.status(200).json({
+        status: true,
+        message: "Admin logged in successfully",
+      });
     } catch (err) {
       return res.status(err.code || 500).json({
         status: false,
