ba35782aba2fef9f52a74620cfaea318c4e3e3a1
Yahyap <yahyap20@gmail.com>
2023-07-28 14:58:03 +0700

    Tambah middleware session

--- backend/middleware/session.js
+++ backend/middleware/session.js
@@ -1,11 +1,20 @@
 const session = require("express-session");
+const express = require("express");
+const app = express();
+
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 const requireAuth = (req, res, next) => {
   if (req.session.authenticated) {
     next();
-  } else {
-    res.redirect("/login");
   }
+  return res.status(401).json({ error: "Unauthorized" });
 };
 
 module.exports = { requireAuth };
