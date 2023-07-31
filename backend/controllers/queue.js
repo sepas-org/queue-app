cef2ab739190ca6781e2f2b29869154e63269198
Muhamad Zulfan <73164719+neunicorn@users.noreply.github.com>
2023-07-31 09:56:27 +0700

    session and queue OK

--- backend/controllers/queue.js
+++ backend/controllers/queue.js
@@ -1,12 +1,14 @@
-class Queue {
-  constructor() {
-    this.queueValue = 0;
-    this.queue = [];
-  }
+var queue = [];
+var queueValue = 0;
 
+class Queue {
   async nextQueue(req, res) {
+    /**
+     * untuk dapetin antrian selanjutnya
+     * @api {get} /api/queue/next Get Next Queue
+     */
     try {
-      let data = this.queue.shift();
+      let data = queue.shift();
       if (!data) {
         throw { code: 400, message: "No queue" };
       }
@@ -25,11 +27,12 @@ class Queue {
 
   async addQueue(req, res) {
     try {
-      this.queueValue++;
-      this.queue.push(this.queueValue);
+      queueValue++;
+      let data = queue.push(queueValue);
       return res.status(200).json({
         status: true,
-        message: `antrian ke ${this.queueValue}`,
+        message: "QUEUE_ADDED",
+        data,
       });
     } catch (err) {
       return res.status(err.code || 500).json({
--- backend/middleware/session.js
+++ backend/middleware/session.js
@@ -1,20 +1,12 @@
 const session = require("express-session");
 const express = require("express");
-const app = express();
-
-app.use(
-  session({
-    secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: true,
-  })
-);
 
 const requireAuth = (req, res, next) => {
   if (req.session.authenticated) {
     next();
+  } else {
+    return res.status(401).json({ error: "Unauthorized" });
   }
-  return res.status(401).json({ error: "Unauthorized" });
 };
 
 module.exports = { requireAuth };
