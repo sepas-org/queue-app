9a76863f8d61fd91cd2f4ddbb73406639e648d02
Muhamad Zulfan <73164719+neunicorn@users.noreply.github.com>
2023-08-07 08:59:41 +0700

    fix route

--- backend/routes/api/queue.js
+++ backend/routes/api/queue.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const queueController = require("../../controllers/queue");
 const { requireAuth } = require("../../middleware/session");
 
-router.get("/next", requireAuth, displayController.nextQueue);
-router.post("/add", displayController.addQueue);
+router.get("/next", requireAuth, queueController.takeQueue);
+router.post("/add", queueController.addQueue);
+router.post("/done", requireAuth, queueController.doneQueue);
+router.delete("/cancel", requireAuth, queueController.cancelQueue);
 
 module.exports = router;
