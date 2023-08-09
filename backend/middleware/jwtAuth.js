c6c9364c2f7b4672767dbacdb98f05d82576733d
Yahyap <yahyap20@gmail.com>
2023-08-09 10:47:42 +0700

    rename dan ubah Session ke Jwt

---
+++ backend/middleware/jwtAuth.js
@@ -0,0 +1,33 @@
+const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
+dotenv.config();
+
+const jwtAuth = () => {
+  try {
+    return async (req, res, next) => {
+      const authorization = req.headers.authorization;
+
+      if (!authorization) {
+        return res.status(401).json({ error: "Unauthorized" });
+      }
+
+      const token = authorization.split(" ")[1];
+      jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).json({ error: "Invalid token" });
+        }
+        req.jwt = decoded;
+        const { username } = decoded;
+        console.log(username);
+        next();
+      });
+    };
+  } catch (err) {
+    return res.status(err.code).json({
+      status: "Failed",
+      message: err.message,
+    });
+  }
+};
+
+module.exports = jwtAuth;
