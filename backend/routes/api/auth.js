bdd3a1a2ecccd275f5ca6ddbbbfc0788b011b918
Muhamad Zulfan <73164719+neunicorn@users.noreply.github.com>
2023-08-10 14:58:05 +0700

    add multer on routes

--- backend/routes/api/auth.js
+++ backend/routes/api/auth.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const upload = multer();
 const Admin = require("../../controllers/admin");
 
 router.post("/register", Admin.register);
-router.post("/login", Admin.login);
+router.post("/login", upload.none(), Admin.login);
 router.get("/logout", Admin.logout);
 
 module.exports = router;
--- backend/routes/api/queue.js
+++ backend/routes/api/queue.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const multer = require("multer");
+const upload = multer();
 const router = express.Router();
 const queueController = require("../../controllers/queue");
 const jwtAuth = require("../../middleware/jwtAuth");
 
 router.get("/next", jwtAuth(), queueController.takeQueue);
-router.post("/add", queueController.addQueue);
-router.post("/done", jwtAuth(), queueController.doneQueue);
+router.post("/add", upload.none(), queueController.addQueue);
+router.post("/done", jwtAuth(), upload.none(), queueController.doneQueue);
 router.delete("/cancel", jwtAuth(), queueController.cancelQueue);
 
 module.exports = router;
